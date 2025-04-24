19a8a628792b1051274b1224c81198e022a41914
Bence Kovacs <kov.benc@gmail.com>
2025-04-24 19:32:02 +0200

    Update GetMediaPlugin.java

--- src/android/GetMediaPlugin.java
+++ src/android/GetMediaPlugin.java
@@ -18,24 +18,9 @@ public class GetMediaPlugin extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
         if ("getLast".equals(action)) {
             final int limit = args.getInt(0);
-            cordova.getThreadPool().execute(new Runnable() {
-                public void run() {
-                    try {
-                        JSONArray result = fetchLast(limit);
-                        callbackContext.success(result);
-                    } catch (Exception e) {
-                        callbackContext.error(e.getMessage());
-                    }
-                }
-            });
-            return true;
-        }
-
-        if ("getLastAsBase64".equals(action)) {
-            final int limit = args.getInt(0);
             cordova.getThreadPool().execute(() -> {
                 try {
-                    JSONArray result = fetchLastAsBase64(limit);
+                    JSONArray result = fetchLast(limit);
                     callbackContext.success(result);
                 } catch (Exception e) {
                     callbackContext.error(e.getMessage());
@@ -47,40 +32,7 @@ public class GetMediaPlugin extends CordovaPlugin {
         return false;
     }
 
-    private JSONArray fetchLast(int limit) throws JSONException {
-        JSONArray array = new JSONArray();
-        Context context = cordova.getActivity().getApplicationContext();
-        Uri uri = MediaStore.Files.getContentUri("external");
-        String[] projection = {
-                MediaStore.Files.FileColumns._ID,
-                MediaStore.Files.FileColumns.MIME_TYPE
-        };
-        String selection = MediaStore.Files.FileColumns.MEDIA_TYPE + "="
-                + MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
-        String sortOrder = MediaStore.Files.FileColumns.DATE_TAKEN + " DESC";
-        Cursor cursor = context.getContentResolver().query(uri, projection, selection, null, sortOrder);
-
-        if (cursor != null) {
-            int fetched = 0;
-            while (cursor.moveToNext() && fetched < limit) {
-                long id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns._ID));
-                String mime = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Files.FileColumns.MIME_TYPE));
-                Uri contentUri = (mime.startsWith("image"))
-                        ? Uri.withAppendedPath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, String.valueOf(id))
-                        : Uri.withAppendedPath(MediaStore.Video.Media.EXTERNAL_CONTENT_URI, String.valueOf(id));
-                JSONObject obj = new JSONObject();
-                obj.put("id", id);
-                obj.put("mimeType", mime);
-                obj.put("uri", contentUri.toString());
-                array.put(obj);
-                fetched++;
-            }
-            cursor.close();
-        }
-        return array;
-    }
-
-    private JSONArray fetchLastAsBase64(int limit) throws JSONException, IOException {
+    private JSONArray fetchLast(int limit) throws JSONException, IOException {
         JSONArray array = new JSONArray();
         Context ctx = cordova.getActivity().getApplicationContext();
         Uri uri = MediaStore.Files.getContentUri("external");
