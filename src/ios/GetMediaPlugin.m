65cbe40d413f4df64da2343b46702c99c450bded
Bence Kovacs <kov.benc@gmail.com>
2025-04-24 18:27:52 +0200

    Update GetMediaPlugin.m

--- src/ios/GetMediaPlugin.m
+++ src/ios/GetMediaPlugin.m
@@ -1,39 +1,41 @@
 #import "GetMediaPlugin.h"
 #import <Photos/Photos.h>
 
-@interface GetMediaPlugin : CDVPlugin
-- (void)getLast:(CDVInvokedUrlCommand*)command;
-@end
-
 @implementation GetMediaPlugin
 
 - (void)getLast:(CDVInvokedUrlCommand*)command {
-    NSNumber *limit = [command.arguments objectAtIndex:0];
+    NSInteger limit = [command.arguments[0] integerValue];
     PHFetchOptions *options = [[PHFetchOptions alloc] init];
     options.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"creationDate" ascending:NO]];
-    options.fetchLimit = [limit integerValue];
-    PHFetchResult *results = [PHAsset fetchAssetsWithOptions:options];
-    NSMutableArray *items = [NSMutableArray array];
-    PHImageManager *manager = [PHImageManager defaultManager];
+    options.fetchLimit = limit;
 
+    // Photos only
+    PHFetchResult<PHAsset *> *results = [PHAsset fetchAssetsWithMediaType:PHAssetMediaTypeImage options:options];
+    NSMutableArray *items = [NSMutableArray array];
     dispatch_group_t group = dispatch_group_create();
 
     for (PHAsset *asset in results) {
         dispatch_group_enter(group);
-        PHContentEditingInputRequestOptions *inputOptions = [[PHContentEditingInputRequestOptions alloc] init];
-        [asset requestContentEditingInputWithOptions:inputOptions completionHandler:^(PHContentEditingInput *contentInput, NSDictionary *info) {
-            NSURL *url = contentInput.fullSizeImageURL ?: (NSURL *)contentInput.audiovisualAsset;
-            NSString *uti = contentInput.uniformTypeIdentifier;
-            if (url) {
-                [items addObject:@{@"uri": url.absoluteString, @"uti": uti ?: @""}];
-            }
+        NSString *localId = asset.localIdentifier;
+        PHContentEditingInputRequestOptions *inputOpts = [[PHContentEditingInputRequestOptions alloc] init];
+        [asset requestContentEditingInputWithOptions:inputOpts completionHandler:^(PHContentEditingInput *input, NSDictionary *info) {
+            NSURL *url = input.fullSizeImageURL;
+            NSString *mime = input.uniformTypeIdentifier ?: @"";
+            NSData *data = url ? [NSData dataWithContentsOfURL:url] : nil;
+            NSString *b64 = data ? [data base64EncodedStringWithOptions:0] : @"";
+            NSDictionary *dict = @{
+                @"id": localId,
+                @"mimeType": mime,
+                @"base64": b64
+            };
+            [items addObject:dict];
             dispatch_group_leave(group);
         }];
     }
 
     dispatch_group_notify(group, dispatch_get_main_queue(), ^{
-        CDVPluginResult *result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:items];
-        [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
+        CDVPluginResult *res = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:items];
+        [self.commandDelegate sendPluginResult:res callbackId:command.callbackId];
     });
 }
 
